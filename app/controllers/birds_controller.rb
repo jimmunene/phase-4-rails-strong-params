e97ccf9c791d085f8c0bcbe0b9a01b0bef78ec27
jimbrian <ui.ux.jimbrian@gmail.com>
2022-11-20 18:13:12 +0300

    rails strong params

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,7 +8,11 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    # bird = Bird.create(name: params[:name], species: params[:species])
+    # What we can do instead is use [Strong Parameters][strong params] to **permit**_only_ the parameters that we want to use:
+    # bird = Bird.create(params.permit(:name, :species))
+    # When we call `params.permit(:name, :species)`, this will return a new hash with **only** the name and species keys. Rails will also mark this new hash as `permitted`, which means we can safely use this new hash for mass assignment.
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -20,6 +24,14 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    byebug
+    params.permit(:name, :species)
   end
 
 end
